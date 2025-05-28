67f1f2ca32ba4dc0bd8c81d362051eccbc987169
Dillon R. Gardner <dillon@gardner.fyi>
2025-05-28 17:59:32 +0200

    add raw data

--- explore.py
+++ explore.py
@@ -1,26 +1,35 @@
 #%%
 
-import polars as pl
+import pandas as pd
 import format
 import constants
 
 
-board1 = pl.read_csv("./figures/iteration_1/formatted_board.csv")
-board2 = pl.read_csv("./figures/iteration_2/formatted_board.csv")
-board3 = pl.read_csv("./figures/iteration_3/formatted_board.csv")
-selections1 = pl.read_csv("./figures/iteration_1/formatted_selections.csv")
-selections2 = pl.read_csv("./figures/iteration_2/formatted_selections.csv")
-selections3 = pl.read_csv("./figures/iteration_3/formatted_selections.csv")
+# board1 = pl.read_csv("./figures/iteration_1/formatted_board.csv")
+# board2 = pl.read_csv("./figures/iteration_2/formatted_board.csv")
+# board3 = pl.read_csv("./figures/iteration_3/formatted_board.csv")
+# selections1 = pl.read_csv("./figures/iteration_1/formatted_selections.csv")
+# selections2 = pl.read_csv("./figures/iteration_2/formatted_selections.csv")
+# selections3 = pl.read_csv("./figures/iteration_3/formatted_selections.csv")
 
 
 
-df1 = format.combine(selections=selections1, board=board1)
-df2 = format.combine(selections=selections2, board=board2)
-df3 = format.combine(selections=selections3, board=board3)
+
+
+df1 = pd.read_csv("./figures/iteration_1/full_selections.csv")
+df2 = pd.read_csv("./figures/iteration_2/full_selections.csv")
+df3 = pd.read_csv("./figures/iteration_3/full_selections.csv")
 
 #%%
+group_cols = ["Training/ Spontaneous",
+              "Utterance: Single word or phrase",
+              "Type of Sign",]
 
-if __name__ == "__main__":
-    "hello"
+#%%
+import seaborn as sns
+
+sns.barplot()
+
+
 
 
